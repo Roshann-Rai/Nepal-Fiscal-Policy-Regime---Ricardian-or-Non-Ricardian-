594d1d41422928b069a994f769f70b9b244e957b
Roshan Rai <101456468+Roshann-Rai@users.noreply.github.com>
2026-09-07 06:23:56 +0545

    additions

--- analysis.R
+++ analysis.R
@@ -31,10 +31,70 @@ df1 <- df%>%
   ) %>%
   filter(year >= 2003)
 
+# Plot
+plot1 <- ggplot(df1, aes(year)) +
+  # geom_line(aes(y = pb_gdp, color = "black")) +
+  geom_line(aes(y = debt_gdp, color = "red"))
+
+plot1
+
+# Structural Breaks Bai - Perron
+break_yr <- breakpoints(pb_gdp ~ debt_gdp, data = df1, h = 0.15)
+summary(break_yr)
+plot(break_yr)
+
+break_forced <- breakpoints(break_yr, breaks = 1)
+df1$year[break_forced$breakpoints]
+confint(break_forced, breaks = 1)
+
+# Chow test
+break_index <- which(df1$year == 2016)
+chow_test <- sctest(pb_gdp ~ debt_gdp, type = "Chow", data = df1, point = break_index)
+print(chow_test)
+
+# years_to_test <- c(2015, 2016, 2017)
+# results <- sapply(years_to_test, function(yr) {
+#   mydata$brk <- as.numeric(mydata$year >= yr)
+#   full    <- lm(y ~ x1 + x2 * brk, data = mydata)
+#   reduced <- lm(y ~ x1 + x2, data = mydata)
+#   a <- anova(reduced, full)
+#   c(F = a$F[2], p = a$`Pr(>F)`[2])
+# })
+# colnames(results) <- years_to_test
+# results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Time series object
-data_cols <- df1[, c("pb_gdp", "debt_gdp")]
-finance_ts <- ts(
-  data = data_cols,
-  start = df$year,
-  frequency = 1
-)
+# data_cols <- df1[, c("pb_gdp", "debt_gdp")]
+# finance_ts <- ts(
+#   data = data_cols,
+#   start = df$year,
+#   frequency = 1
+# )
+
+
+
+
+
+
+
+
+
+
+
+
+
